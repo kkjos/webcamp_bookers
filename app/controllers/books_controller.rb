b7a52500b80a44939c971b1d50d9018e49f988ee
kkjos <k19951227@gmail.com>
2021-10-10 04:37:55 +0000

    [add]投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,21 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book =  Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_show_path(params[:id])
   end
 
   def edit
   end
+  
+  private
+  def book_params
+    params.reqiure(:Book).permit(:title, :body)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/edit'
+  get 'top' => 'homes#top'
+
+  resources :books
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

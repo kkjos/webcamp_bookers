cc6172d9e0f1a7181779d2b3009168515f2f0803
kkjos <k19951227@gmail.com>
2021-10-10 07:29:13 +0000

    [error]投稿

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,30 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
+    blog.save
+    redirect_to book_path
   end
 
   def new
-    @book =  Book.new
+    @book = Book.new
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_show_path(params[:id])
+    redirect_to books_path
   end
 
   def edit
   end
-  
+
   private
   def book_params
     params.reqiure(:Book).permit(:title, :body)
   end
-  
+
 end

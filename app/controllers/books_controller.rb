d3a446c22ef444d90b2fb03f197710831d47df88
kkjos <k19951227@gmail.com>
2021-10-11 05:28:26 +0000

    [add]hhead/hbody

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,20 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-  end
-
   def create
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = 'Book was successfully created.'
-      redirect_to book_path(@book.id)
+      redirect_to books_path(@book.id)
     else
-      render :new
+      render :index
     end
   end
 
